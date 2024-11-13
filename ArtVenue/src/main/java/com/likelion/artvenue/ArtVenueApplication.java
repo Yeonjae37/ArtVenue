e7dee058e04c23e27f1153c704f27919eff471b6
Yeonjae37 <jyj030307@gmail.com>
2024-11-13 09:58:03 +0900

    fist commit

---
+++ ArtVenue/src/main/java/com/likelion/artvenue/ArtVenueApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.artvenue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtVenueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtVenueApplication.class, args);
+	}
+
+}
---
+++ ArtVenue/src/test/java/com/likelion/artvenue/ArtVenueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.artvenue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtVenueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
